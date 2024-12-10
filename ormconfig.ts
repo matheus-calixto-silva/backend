a1afad192703d2eaa4d5ac44e300da7bc487ee85
matheus-calixto-silva <matheuscalixto8@gmail.com>
2024-12-10 19:21:58 -0300

    refactor: ajuste no import do ormconfig.ts

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,17 +1,16 @@
-import { DataSourceOptions } from 'typeorm';
-import { env } from './src/app/config/env';
+import { DataSource } from 'typeorm';
+import { env } from './src/app/config/env.ts';
 
-const ormconfig: DataSourceOptions = {
+export const AppDataSource = new DataSource({
   type: 'mysql',
   host: 'localhost',
   port: parseInt(env.mysql.port, 10),
   username: env.mysql.username,
   password: env.mysql.password,
   database: env.mysql.database,
-  entities: ['src/app/models/*.ts'],
-  migrations: ['src/database/migrations/*.ts'],
   synchronize: false,
   logging: false,
-};
-
-export default ormconfig;
+  entities: ['src/app/models/*.ts'],
+  migrations: ['src/database/migrations/*.ts'],
+  subscribers: [],
+});
