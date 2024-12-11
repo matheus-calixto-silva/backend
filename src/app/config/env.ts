10c6afbf1100177953aca8f7198eed7b0ef39448
matheus-calixto-silva <matheuscalixto8@gmail.com>
2024-12-11 17:56:52 -0300

    feat: adiciona propriedade secretKey ao env.ts

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT!;
+const SECRET_KEY = process.env.SECRET_KEY!;
 
 const MYSQL_PORT = process.env.MYSQL_PORT!;
 const MYSQL_USERNAME = process.env.MYSQL_USERNAME!;
@@ -9,6 +10,7 @@ const MYSQL_DATABASE = process.env.MYSQL_DATABASE!;
 
 export const env = {
   port: PORT,
+  secretKey: SECRET_KEY,
   mysql: {
     port: MYSQL_PORT,
     username: MYSQL_USERNAME,
