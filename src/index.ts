acc39856d0b6fc298924206e4b8319276e87f3cd
matheus-calixto-silva <matheuscalixto8@gmail.com>
2024-12-11 18:20:11 -0300

    feat: chamando controllers no index

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 import 'reflect-metadata';
+import './database/connect.ts';
 
 import { env } from './app/config/env.ts';
-import './database/connect.ts';
-import router from './routes.ts';
+import { loginRouter } from './app/controllers/loginController.ts';
+import { servicesRouter } from './app/controllers/serviceController.ts';
+import { usersRouter } from './app/controllers/userController.ts';
 
 const app = express();
 
 app.use(express.json());
-app.use(router);
+app.use(loginRouter);
+app.use(usersRouter);
+app.use(servicesRouter);
 
 app.listen(env.port, () =>
   console.log(`🔥 Servidor rodando em http://localhost:${env.port}`)
