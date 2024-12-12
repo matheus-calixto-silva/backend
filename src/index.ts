6f35849eb7dafbe91b28fb2b27c3ad6d84d46281
matheus-calixto-silva <matheuscalixto8@gmail.com>
2024-12-12 00:09:26 -0300

    feat: chamando controller de agendamento no index

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
+import cors from 'cors';
 import express from 'express';
 import 'reflect-metadata';
 import './database/connect.ts';
 
 import { env } from './app/config/env.ts';
 import { loginRouter } from './app/controllers/loginController.ts';
+import { schedulesRouter } from './app/controllers/scheduleController.ts';
 import { servicesRouter } from './app/controllers/serviceController.ts';
 import { usersRouter } from './app/controllers/userController.ts';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(loginRouter);
 app.use(usersRouter);
 app.use(servicesRouter);
+app.use(schedulesRouter);
 
 app.listen(env.port, () =>
   console.log(`🔥 Servidor rodando em http://localhost:${env.port}`)
